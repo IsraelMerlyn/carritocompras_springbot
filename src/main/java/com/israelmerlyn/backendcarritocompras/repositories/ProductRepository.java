8b5b152d757dadcac6b677a963f791a28f0d8dbd
IsraelMerlyn <merlyn142a@gmail.com>
2024-02-07 12:35:45 -0600

    Feature#7-corriendo-estructura-de-carpetas-front-y-back Agregando frontend y realizando consumo de la API

--- src/main/java/com/israelmerlyn/backendcarritocompras/repositories/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.israelmerlyn.backendcarritocompras.repositories;
-
-import com.israelmerlyn.backendcarritocompras.model.entities.Product;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepository extends CrudRepository<Product, Long> {
-}
